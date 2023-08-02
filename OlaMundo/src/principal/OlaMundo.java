5f8e090e4cacd320242b20fc5882642c085a2c7e
MariaFernandaDev <mariafernanda.calado.07@gmail.com>
2023-08-02 17:34:43 -0300

    subindo arquivos da primeira aula de Java

---
+++ OlaMundo/src/principal/OlaMundo.java
@@ -0,0 +1,20 @@
+package principal;
+
+//classe principal 
+public class OlaMundo {
+
+	//método principal onde executa o programa 
+	public static void main(String[] args) {
+		
+	    // imprime uma linha e ao final salta uma nova linha 
+		// print = imprimir; ln = new line;
+		System.out.println("Hello world!");
+        //comentário de uma linha 
+		/* esse
+		 * é um 
+		 * comentário de
+		 * múltiplas linhas 
+		 * */
+	}
+
+}
