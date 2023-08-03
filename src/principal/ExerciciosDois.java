3b225741648bc71a9de4f6b4f8ebf2cff61a1d08
MariaFernandaDev <mariafernanda.calado.07@gmail.com>
2023-08-03 14:38:03 -0300

    exercício dois de sala

---
+++ src/principal/ExerciciosDois.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class ExerciciosDois {
+
+	public static void main(String[] args) {
+		String nome = "Maria Fernanda";
+		String sobrenome = " Calado de Oliveira";
+		
+		System.out.println(nome + sobrenome);
+	}
+}
